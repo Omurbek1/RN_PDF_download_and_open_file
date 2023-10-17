89a772376bc87139413e8e9e47031740712171d9
Omur <omurbekmamytbekov545@gmail.com>
2023-10-17 13:21:23 +0600

    Open and Download pdf file RN

--- App.tsx
+++ App.tsx
@@ -2,9 +2,9 @@ import {Button, Platform, SafeAreaView, StyleSheet, Text, View} from 'react-nati
 import React from 'react';
 import ReactNativeBlobUtil from 'react-native-blob-util';
 import Share from 'react-native-share';
-
+import FileViewer from "react-native-file-viewer";
+import DocumentPicker from "react-native-document-picker";
 export default function App() {
-  
   const downloadFile = () => {
     const source = "https://www.africau.edu/images/default/sample.pdf";
     let dirs = ReactNativeBlobUtil.fs.dirs;
@@ -38,10 +38,30 @@ export default function App() {
       })
       .catch((err) => console.log('BLOB ERROR -> ', err));
   };
+
+  const openFile=async ()=>{
+    try {
+      const res:any = await DocumentPicker.pickSingle({
+        type: [DocumentPicker.types.pdf],
+        
+      });
+
+      await FileViewer.open(res.uri);
+    } catch (e) {
+      if(DocumentPicker.isCancel(e)){
+        console.log('Document is cancelled')
+      }else{
+        console.log('Document is cancelled')
+      }
+    }
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <Text>Download PDF file</Text>
       <Button title="download" onPress={downloadFile} />
+      <Text>Open PDF file</Text>
+      <Button title="Open Pdf " onPress={openFile} />
     </SafeAreaView>
   );
 }
